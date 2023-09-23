31318b77e52d7ceaf47d61bc9a65371469e1f6a4
AbdelrahmanAhmed26 <abdo2642001@gmail.com>
2023-09-23 08:10:06 +0300

    Day 7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MovieComponent } from './movie/movie.component';
+import { TvComponent } from './tv/tv.component';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { TvDetailsComponent } from './tv-details/tv-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // /home
+  {path:'home',component: HomeComponent},
+  {path:'',component: HomeComponent},
+  {path:'movies',component: MovieComponent},
+  {path:'moviesdetails/:id',component: MovieDetailsComponent},
+  {path:'tvs',component: TvComponent},
+  {path:'tvsdetails/:id',component: TvDetailsComponent},
+  {path:'about',component: AboutComponent},
+  {path:'**',component: PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
